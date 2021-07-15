49452a407d63a6e9b8b89c19d373015c11a71749
Jessica Antunes <jessantunes@protonmail.com>
2021-07-14 21:41:58 -0400

    add router and switch

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ import Navbar from './components/Navbar';
 class App extends Component {
   render() {
     return (
-      <>
-      <Navbar />
-      </>
+      <Router>
+      <NavBar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={ Home } />
+            <Route exact path="/pets" component={PetList} />
+            <Route exact path="/artworks" component={ArtworkList} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
