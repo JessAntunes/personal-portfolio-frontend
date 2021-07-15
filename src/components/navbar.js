bea9bcd0de67087c472691ca515230289a9f91c1
Jessica Antunes <jessantunes@protonmail.com>
2021-07-14 21:10:59 -0400

    change all navbar structure code

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,37 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
 
-export default function ButtonAppBar() {
-  const classes = useStyles();
 
-  return (
-    <div className={classes.root} id="navbar">
-      <AppBar style={{ background: '#424242' }} position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Jessica Antunes
-          </Typography>
-          <Button color="inherit">Home</Button>
-          <Button color="inherit">Projects</Button>
-          <Button color="inherit">Artwork</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <nav className="indigo darken-3">
+        <div className="container">
+          <NavLink to="/" className="brand-logo">Jessica Antunes</NavLink>
+          <ul className="right">
+          <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/projects">Project</NavLink></li>
+            <li><NavLink to="/artworks">Artwork</NavLink></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
+
+export default Navbar
