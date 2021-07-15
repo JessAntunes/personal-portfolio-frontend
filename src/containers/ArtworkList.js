ca89f50602d6274af3b5a1269d37ff3f46bb7f96
Jessica Antunes <jessantunes@protonmail.com>
2021-07-15 12:39:20 -0400

    change display for artworklist

--- src/containers/ArtworkList.js
+++ src/containers/ArtworkList.js
@@ -1,45 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ArtworkItem from '../components/ArtworkItem'
 import { connect } from 'react-redux'
-import { makeStyles } from '@material-ui/core/styles';
-import ImageList from '@material-ui/core/ImageList';
-import ImageListItem from '@material-ui/core/ImageListItem';
 import { fetchArtworks } from '../actions/index'
-// import itemData from './itemData';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
-  },
-  imageList: {
-    width: 500,
-    height: 450,
-  },
-}));
+
+
 
 
 export class ArtworkList extends Component {
-    
 
     componentDidMount() {
         this.props.fetchArtworks();
     }
 
     render() {
-        const styles = useStyles();
         const artworks = this.props.artworks.map(( artwork, i ) => ({artwork}))
         return (
-            <div className={styles.root}>
-            <ImageList rowHeight={160} className={styles.imageList} cols={3}>
-                {artworks.map((item) => (
-                <ImageListItem key={item.img} cols={item.cols || 1}>
-                    <img src={item.img} alt={item.title} />
-                </ImageListItem>
-                ))}
-            </ImageList>
+            <div>
+              <h3>Artwork</h3>
+              <ul className="collection">
+                { artworks }
+              </ul>
+          
             </div>
         );
     }
