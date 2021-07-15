efe4de291219392fac6ded63c6936d57a978203d
Jessica Antunes <jessantunes@protonmail.com>
2021-07-14 20:50:50 -0400

    create about component

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+export default function About() {
+    return (
+        <>
+            <h2>About</h2>
+            <p></p>
+            <Button variant="outline-dark" >Artwork</Button>
+
+        </>
+    )
+}
