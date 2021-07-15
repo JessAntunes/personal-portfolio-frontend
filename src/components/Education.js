dd21dfdd8f947b4a2fa052ab038dbf19950fcb1b
Jessica Antunes <jessantunes@protonmail.com>
2021-07-15 00:08:01 -0400

    Create education component for infograph

---
+++ src/components/Education.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export class Education extends Component {
+    render () {
+        return (
+            <div className="education">
+                {/* <img src=" " /> */}
+            </div>
+        )
+    }
+}
+
+export default Education
