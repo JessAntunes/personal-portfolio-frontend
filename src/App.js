487308d8cd87ae64a0a56113dff74f7d6e4a519c
Jessica Antunes <jessantunes@protonmail.com>
2021-07-14 21:27:07 -0400

    create home container, rmove components to home containter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-import Skills from './components/Skills';
-import HomeBanner from './components/HomeBanner';
-import About from './components/About'
+
 
 class App extends Component {
   render() {
     return (
       <>
       <Navbar />
-      <HomeBanner />
-      <Skills />
-      <About />
       </>
     );
   }
---
+++ src/containers/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Skills from './components/Skills';
+import HomeBanner from './components/HomeBanner';
+import About from './components/About'
+
+
+export class Home extends Component {
+  render() {
+    return (
+      <>
+        <HomeBanner />
+        <row className="middle-row">
+            <col className="skills"><Skills /></col>
+            <col className="about"><About /></col>
+        </row>
+      </>
+    )
+  }
+}
+
+export default Home
