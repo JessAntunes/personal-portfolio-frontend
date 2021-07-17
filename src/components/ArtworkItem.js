136ff0a571b6b9b1194e10398829fbc9643c2a22
Jessica Antunes <jessantunes@protonmail.com>
2021-07-17 16:26:38 -0400

    change .img to .photo

--- src/components/ArtworkItem.js
+++ src/components/ArtworkItem.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { connect } from 'react-redux'
+
 
 
 const ArtworkItem = ({ artwork }) => {
     return (
     <li className="collection-item">
-       <img src={artwork.img} alt={artwork.type} />
+       <img src={artwork.photo} alt={artwork.type} />
     </li>
     );
 }
 
 
-export default connect(null)(ArtworkItem)
+export default ArtworkItem
 
 
 
