38319984d7a74ce6ad885ac0d66686e032e1c6f0
Jessica Antunes <jessantunes@protonmail.com>
2021-07-20 13:36:30 -0400

    link button to correct location

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export class About extends Component {
             <>
                 <h2>About</h2>
                 <p>Jessica is experienced in Ruby on Rails and JavaScript based programming with a background in Graphic Design, Information Sys- tems Security, Logistics, and Customer Service. Possess strong skills in customer service, problem solving, and computer diagnostics that have helped companies gain customer trust and improved customer retention. Currently a Full Stack Software Engineer student dedicated to engineering user-friendly, thoughtful web applications.</p>
-                <Button variant="outline-dark" >Artwork</Button>
+                <Button variant="outline-dark" href="/artworks">Artwork</Button>
             </>
         )
     }
