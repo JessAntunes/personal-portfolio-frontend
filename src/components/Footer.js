01d4a45bb90aabbcd1b9175dee9476dc5ef754b2
Jessica Antunes <jessantunes@protonmail.com>
2021-07-20 13:37:44 -0400

    create top div for footer with contact info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,34 @@
 import React, {Component} from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
 
 
 export class Footer extends Component {
     render () {
         return (
             <>
-        <Navbar bg="dark" variant="dark">
-          <Container>
-          <Navbar.Text href="/">© 2021 Jessica Antunes. All rights reserved</Navbar.Text>
-          <Nav className="me-right">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="/artworks">Artwork</Nav.Link>
-          </Nav>
-          </Container>
-        </Navbar>
+                <div className="contact-container">
+                    <Row className="contact-row">
+                        <Col className="contact-info">
+                            <h5>Contact Info</h5>
+                            <p>Email: jessantunes@protonmail.com</p>
+                            <p>LinkedIn: https://www.linkedin.com/in/jessica-antunes/</p>
+                        </Col>
+                        <Col className="custom-contact">
+                            <h5>Custom Artwork</h5>
+                            <p>I am currently not taking inquiries about custom artwork, purchase prints, or items shown under the artwork page.</p>
+                        </Col>
+                    </Row>
+                </div>
+                <Navbar bg="dark" variant="dark">
+                <Container>
+                <Navbar.Text href="/">© 2021 Jessica Antunes. All rights reserved</Navbar.Text>
+                <Nav className="me-right">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/projects">Projects</Nav.Link>
+                    <Nav.Link href="/artworks">Artwork</Nav.Link>
+                </Nav>
+                </Container>
+                </Navbar>
       </>
         )
     }
