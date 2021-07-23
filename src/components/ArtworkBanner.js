ed34f389791b3af34aaf510643bba7ace692e83c
Jessica Antunes <jessantunes@protonmail.com>
2021-07-23 12:52:41 -0400

    created banner component for artwork

---
+++ src/components/ArtworkBanner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ArtworkBanner = () => {
+    return (
+        <>
+            <div className="home-banner"></div>
+            <div className="home-text">
+                <h1>Artwork</h1>
+            </div>
+        </>
+    )
+}
+
+export default ArtworkBanner
